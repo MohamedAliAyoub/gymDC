66239ee7711ae458c4478c229653402b8ec7dba3
MohamedAliAyoub <mohamedali163163@gmail.com>
2024-05-18 00:32:24 +0200

    [FEAT] make  tables in migration files

---
+++ database/migrations/2024_05_17_175953_create_standard_types_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('standard_types', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->foreignId('standard_id')->constrained()->cascadeOnDelete();
+            $table->boolean("status")->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('standard_types');
+    }
+};
