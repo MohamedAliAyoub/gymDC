3d58daa9384a77c579c5bdab21557ec5e4b79149
MohamedAliAyoub <mohamedali163163@gmail.com>
2024-05-25 21:43:23 +0200

    [Feat] add migration file of exercise tables

---
+++ database/migrations/2024_05_25_121023_create_exercise_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('exercise_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->tinyInteger('previous')->nullable();
+            $table->tinyInteger('rir')->nullable();
+            $table->string('tempo')->nullable();
+            $table->tinyInteger('rest')->nullable();
+            $table->tinyInteger('kg')->nullable();
+            $table->tinyInteger('reps')->nullable();
+            $table->boolean('status')->default(true);
+            $table->foreignId('exercise_id')->constrained()->onDelete('cascade');
+            $table->boolean('is_run')->default(false);
+            $table->string('run_duration')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exercise_details');
+    }
+};
