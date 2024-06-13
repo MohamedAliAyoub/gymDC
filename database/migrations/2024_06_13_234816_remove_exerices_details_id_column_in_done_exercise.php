280cdf52672ba84084206c19bd31445a2b8e9aef
MohamedAliAyoub <mohamedali163163@gmail.com>
2024-06-14 01:53:15 +0200

    [MOD] remove unused coloumns

---
+++ database/migrations/2024_06_13_234816_remove_exerices_details_id_column_in_done_exercise.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('done_exercises', function (Blueprint $table) {
+            $table->dropForeign(['exercise_details_id' ]);
+            $table->dropForeign(['plan_id' ]);
+            $table->dropColumn('exercise_details_id');
+            $table->dropColumn('plan_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('done_exercises', function (Blueprint $table) {
+            $table->foreignId('exercise_details_id')->nullable();
+            $table->foreignId('plan_id')->nullable();
+        });
+    }
+};
