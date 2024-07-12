2501c515c93f3c5a6fe97f7493f226ff83000d64
MohamedAliAyoub <mohamedali163163@gmail.com>
2024-07-12 15:16:54 +0200

    [FEAT] update docs

--- app/Http/Controllers/Dashboard/SubscriptionController.php
+++ app/Http/Controllers/Dashboard/SubscriptionController.php
@@ -14,26 +14,26 @@ class SubscriptionController extends Controller
 {
 
 
-   /**
- * @OA\Schema(
- *     schema="Subscription",
- *     type="object",
- *     @OA\Property(property="id", type="integer", example=1),
- *     @OA\Property(property="nutrition_coach_id", type="integer", example=1),
- *     @OA\Property(property="workout_coach_id", type="integer", example=1),
- *     @OA\Property(property="client_id", type="integer", example=1),
- *     @OA\Property(property="sale_id", type="integer", example=1),
- *     @OA\Property(property="duration", type="integer", example=30),
- *     @OA\Property(property="status", type="boolean", example=true),
- *     @OA\Property(property="package", type="string", example="Basic"),
- *     @OA\Property(property="started_at", type="string", format="date", example="2022-01-01"),
- *     @OA\Property(property="paid_amount", type="number", format="float", example=100.00),
- *     @OA\Property(property="freeze_start_at", type="string", format="date", example="2022-01-01"),
- *     @OA\Property(property="freeze_duration", type="integer", example=7),
- *     @OA\Property(property="paid_at", type="string", format="date", example="2022-01-01"),
- *     @OA\Property(property="created_at", type="string", format="date", example="2022-01-01"),
- * )
- */
+    /**
+     * @OA\Schema(
+     *     schema="Subscription",
+     *     type="object",
+     *     @OA\Property(property="id", type="integer", example=1),
+     *     @OA\Property(property="nutrition_coach_id", type="integer", example=1),
+     *     @OA\Property(property="workout_coach_id", type="integer", example=1),
+     *     @OA\Property(property="client_id", type="integer", example=1),
+     *     @OA\Property(property="sale_id", type="integer", example=1),
+     *     @OA\Property(property="duration", type="integer", example=30),
+     *     @OA\Property(property="status", type="boolean", example=true),
+     *     @OA\Property(property="package", type="string", example="Basic"),
+     *     @OA\Property(property="started_at", type="string", format="date", example="2022-01-01"),
+     *     @OA\Property(property="paid_amount", type="number", format="float", example=100.00),
+     *     @OA\Property(property="freeze_start_at", type="string", format="date", example="2022-01-01"),
+     *     @OA\Property(property="freeze_duration", type="integer", example=7),
+     *     @OA\Property(property="paid_at", type="string", format="date", example="2022-01-01"),
+     *     @OA\Property(property="created_at", type="string", format="date", example="2022-01-01"),
+     * )
+     */
     public function index(): JsonResponse
     {
         $subscriptions = Subscription::query()->with(
@@ -50,7 +50,6 @@ class SubscriptionController extends Controller
             'data' => SubscriptionResource::collection($subscriptions),
         ]);
     }
-
 
     /**
      * @OA\Get(
@@ -72,6 +71,7 @@ class SubscriptionController extends Controller
      *             @OA\Property(property="message", type="string", example="client subscriptions fetched successfully"),
      *             @OA\Property(property="count", type="integer", example=10),
      *             @OA\Property(property="data", type="array", @OA\Items(ref="#/components/schemas/Subscription")),
+     *             @OA\Property(property="active_subscription", type="object", @OA\Schema(ref="#/components/schemas/Subscription")),
      *             @OA\Property(property="user_details", type="object",
      *                 @OA\Property(property="id", type="integer", example=1),
      *                 @OA\Property(property="name", type="string", example="John Doe"),
@@ -79,7 +79,8 @@ class SubscriptionController extends Controller
      *                 @OA\Property(property="phone", type="string", example="1234567890"),
      *                 @OA\Property(property="age", type="integer", example=30),
      *                 @OA\Property(property="weight", type="integer", example=70),
-     *                 @OA\Property(property="height", type="integer", example=180)
+     *                 @OA\Property(property="height", type="integer", example=180),
+     *                 @OA\Property(property="in_body_image", type="string", example="image_url")
      *             )
      *         )
      *     ),
@@ -101,17 +102,16 @@ class SubscriptionController extends Controller
     {
         //TODO solve error of client not found
 
-     $query = Subscription::query()
-    ->where('client_id', $request->id)
-    ->with(['client' => function ($q) {
-        $q->with('userDetails', function ($q) {
-            $q->latest();
-        });
-    }])
-    ->orderByDesc('id');
+        $query = Subscription::query()
+            ->where('client_id', $request->id)
+            ->with(['client' => function ($q) {
+                $q->with('userDetails', function ($q) {
+                    $q->latest();
+                });
+            }])
+            ->orderByDesc('id');
 
-     $subscriptions = $query->paginate(15);
-
+        $subscriptions = $query->paginate(15);
 
 
         $activeSubscription = $query->where('status', 1)
