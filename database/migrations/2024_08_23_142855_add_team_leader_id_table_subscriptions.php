e447311b411e2fa78c002701aa46412b305b4ed7
MohamedAliAyoub <mohamedali163163@gmail.com>
2024-09-21 13:48:46 +0200

    team leader in subscription & user check

--- database/migrations/2024_08_23_142855_add_team_leader_id_table_subscriptions.php
+++ database/migrations/2024_08_23_142855_add_team_leader_id_table_subscriptions.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('subscriptions', function (Blueprint $table) {
-            if(!Schema::hasColumn('subscriptions', 'team_leader_id'))
+            if (!Schema::hasColumn('subscriptions', 'team_leader_id'))
                 $table->foreignId('team_leader_id')->nullable()->constrained('users')->onDelete('cascade');
         });
     }
