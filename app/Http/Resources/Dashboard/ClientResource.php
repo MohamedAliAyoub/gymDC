01f54eb222db7d6260fa66c2c6222a360206a3db
Mohamed.a.ayoub <Mohamed.a.ayoub@te.eg>
2024-10-06 16:54:27 +0300

    add image in client resource

--- app/Http/Resources/Dashboard/ClientResource.php
+++ app/Http/Resources/Dashboard/ClientResource.php
@@ -14,6 +14,7 @@ class ClientResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
+            'image' => $this->image_url,
             'mobile' => $this->mobile,
             'created_at' => $this->created_at->format('Y-m-d'),
             'forms_updated_at' => $this->activeSubscription?->updated_at->format('Y-m-d') ?? $this->activeSubscription?->created_at->format('Y-m-d'),
